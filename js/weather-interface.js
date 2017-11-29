9755ecff0ddd79d76b715b8c75559e3c29613b9b
Jessie Waite and Valia Dempsey <valia@>
2017-11-29 10:09:17 -0800

    Add .env file. Since lacking gulp file and packages placed variable for apiKey in interface.js file for now. Use require with regular gulp setup.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,9 +1,10 @@
+let apiKey = "b6e67a135ccdd8ad45efcfbc13bff55e";
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
     $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b6e67a135ccdd8ad45efcfbc13bff55e`,
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
       type: 'GET',
       data: {
         format: 'json'
@@ -12,8 +13,8 @@ $(document).ready(function() {
         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
         $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
       },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
+      error: function(response) {
+        $('.errors').text("There was an error processing your request. Please try again.")
       }
     });
   });
