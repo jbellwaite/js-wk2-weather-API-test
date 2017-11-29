842b92b9364c1b86270a0d699d3d03336eda30dc
Jessie Waite and Valia Dempsey <jessie@>
2017-11-29 10:29:09 -0800

    Add promise syntax to new branch for comparison

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,21 +1,12 @@
-let apiKey = "b6e67a135ccdd8ad45efcfbc13bff55e";
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function(response) {
-        $('.errors').text("There was an error processing your request. Please try again.")
-      }
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b6e67a135ccdd8ad45efcfbc13bff55e`).then(function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }).fail(function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
   });
 });
